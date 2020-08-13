1b28b6792f97940774a8300171336d99b40fba47
Kishore Newton <kishorenke@gmail.com>
2020-08-13 09:18:57 +0530

    localstorage added

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -6,19 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements OnInit {
-  customFilter = [] 
-  storedName = []
-
+  @ViewChild('localStorage') localStorageInput
+  userSetFilters = JSON.parse(localStorage.getItem("userFilter"))
   constructor() { }
 
   ngOnInit() {}
 
-  pushLocalStorage(event){
-    this.customFilter.push(event.value)
-    localStorage.setItem("customFilter", JSON.stringify(this.customFilter))
-    const store = JSON.parse(localStorage.getItem("customFilter"))
-    this.storedName = [...this.storedName, store]
-    console.log(this.storedName)
+  // localStore = localStorage.setItem("userFilter", JSON.stringify([]))
+
+  pushLocalStorage(value){
+    let customFiler = JSON.parse(localStorage.getItem('userFilter'))
+    // customFiler.push(value)
+    // console.log(customFiler)
+    localStorage.setItem("userFilter", JSON.stringify([...customFiler, value]))
+    this.localStorageInput.nativeElement.value = ''
+    console.log(JSON.parse(localStorage.getItem("userFilter")))
   }
 
 }
