821e1b7224c4377d737b3fed5590d742a81b83d0
Kishore Newton <kishorenke@gmail.com>
2021-01-28 15:57:46 +0530

    feat: improved filter

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,83 +1,80 @@
-import {Component, ViewChild, Renderer2, ElementRef} from '@angular/core';
-import Filter from 'bad-words'
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
-import { Observable } from 'rxjs'
-import { map, shareReplay, filter } from 'rxjs/operators'
+import { Component, ViewChild, Renderer2, ElementRef } from "@angular/core";
+import * as Filter from '../badwords'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-
 export class HomePage {
-  @ViewChild('clear') clean
-  @ViewChild('textarea') textarea
-  isShow = true
-  custom = 'Custom'
+  @ViewChild("clear") clean: any;
+  @ViewChild("textarea") textarea: ElementRef;
+  isShow = true;
+  custom = "Custom";
   labels = [
     {
-      color: 'purple',
-      text: 'x'
+      color: "purple",
+      text: "x",
     },
     {
-      color: 'blue',
-      text: '.'
+      color: "blue",
+      text: ".",
     },
     {
-      color: 'green',
-      text: '-'
+      color: "green",
+      text: "-",
     },
     {
-      color: 'yellow',
-      text: '*'
+      color: "yellow",
+      text: "*",
     },
     {
-      color: 'red',
-      text: '_'
+      color: "red",
+      text: "_",
     },
     {
-      color: 'gray',
-      text: 'null'
+      color: "gray",
+      text: "null",
+    },
+  ];
+
+  constructor(private renderer: Renderer2) {}
+
+  filterData(toggle: string, input: string) {
+    if (toggle === "null") toggle = " ";
+    const newBadWords = JSON.parse(localStorage.getItem("userFilter")) || [];
+    const removeWords = JSON.parse(localStorage.getItem("removeFilter")) || [];
+    if (
+      this.clean.nativeElement.value === null ||
+      this.clean.nativeElement.value === ""
+    ) {
+      const customFilter = new Filter({
+        placeHolder: toggle,
+      });
+      customFilter.addWords(...newBadWords);
+      customFilter.removeWords(...removeWords);
+      this.addElement(customFilter.clean(input));
+    } else {
+      const customFilter = new Filter({
+        placeHolder: this.clean.nativeElement.value,
+      });
+      customFilter.addWords(...newBadWords);
+      customFilter.removeWords(...removeWords);
+      this.addElement(customFilter.clean(input));
     }
-  ]
-
-  constructor(private renderer: Renderer2){
-
   }
 
-  filterData(toggle, input) {
-    if(toggle === 'null') toggle = ' '
-    const newBadWords = JSON.parse(localStorage.getItem('userFilter')) || []
-    const removeWords = JSON.parse(localStorage.getItem('removeFilter')) || []
-    if(this.clean.nativeElement.value === null || this.clean.nativeElement.value === ''){
-      const customFilter = new Filter({
-        placeHolder: toggle
-      })
-      customFilter.addWords(...newBadWords)
-      customFilter.removeWords(...removeWords)
-      this.addElement(customFilter.clean(input))
-    } else  {
-      const customFilter = new Filter({
-        placeHolder: this.clean.nativeElement.value
-      })
-      customFilter.addWords(...newBadWords)
-      customFilter.removeWords(...removeWords)
-      this.addElement(customFilter.clean(input))
-    }
-  }
-
-  addElement(text){
-    this.textarea.nativeElement.value = text
+  addElement(text: string) {
+    this.textarea.nativeElement.value = text;
   }
 
   toggleInput() {
-    this.isShow = !this.isShow
-    if(this.isShow === true){
-      this.custom = 'Custom'
-      this.clean.nativeElement.value = ''
+    this.isShow = !this.isShow;
+    if (this.isShow === true) {
+      this.custom = "Custom";
+      this.clean.nativeElement.value = "";
     } else {
-      this.custom = 'Back'
+      this.custom = "Back";
     }
   }
 }
